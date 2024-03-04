03be65b55bce04b0733baf196258d37a2fad7e4c
unknown <gurudarji@ymail.com>
2024-03-04 13:41:17 -0500

    start a new weather app project still a work in progress

---
+++ src/api.js
@@ -0,0 +1,16 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '816e579919msh0150441fc55e0e6p175377jsn0691b2893ecb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "cc08f794994e892d499e3af0e915b375"; 
+
+
+
+    
